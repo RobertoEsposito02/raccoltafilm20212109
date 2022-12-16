748d5f23204c32cf1e927bc93c276b0b9d125fd5
RobertoEsposito02 <robiespo02@libero.it>
2022-12-16 09:12:44 +0100

    modificato auth

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,15 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { Utente } from 'src/app/model/utente';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService{
-
-  constructor() { }
-
+  
+  private userLoggedSubject$: BehaviorSubject<Utente | null> = new BehaviorSubject<Utente | null>(null);
+  
+  constructor(private _http:HttpClient) { }
+  
   login(loginForm:Utente){
-    return of({username: loginForm.username, token:""});
+    return of({username: loginForm.username, token: ""});
+  }
+
+  setUserLogged(user:Utente | null){
+    this.userLoggedSubject$.next(user);
+  }
+
+  getUserLogged():Observable<Utente | null>{
+    return this.userLoggedSubject$.asObservable();
+  }
+
+  isLogged():boolean{
+    return this.userLoggedSubject$.value ? !!this.userLoggedSubject$.value.token : false
+  }
+
+  getUserToken():string | null{
+    return this.userLoggedSubject$.value ? this.userLoggedSubject$.value.token : null
+  }
+
+  logout(){
+    this.setUserLogged(null)
   }
 }
